5ad9bffc5ef632010cb02083043233f6ced11d2f
Slyrox-Analytics <fragtox@web.de>
2025-10-07 20:52:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ with tab_settings:
     # Deine kurze, feste Erklärung oben
     st.markdown(
         """
-**Fensterprinzip (Bewertung):**
+
 - Die KI betrachtet ein Fenster der letzten Messwerte (z. B. 600).
 - Jeder Messwert besteht aus Temperatur, Strom, Spannung, Vibration und Lüfter.
 - Aus den 599 vergangenen Punkten lernt sie das **normale Verhalten**.
